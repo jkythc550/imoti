cbd926237074e1dac093e5820903cb5602045d33
jkythc550 <82351677+jkythc550@users.noreply.github.com>
2021-04-12 10:12:53 +0800

    first init
    first init

---
+++ precache-manifest.0aa847820562352247b473e5ef10ea78.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e1f5c3713955ef6fa52d385b067fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c5a99682aed5776805",
+    "url": "/static/css/2.2ba95c2c.chunk.css"
+  },
+  {
+    "revision": "116d9c29d8578bfb29d6",
+    "url": "/static/css/main.bc598cf8.chunk.css"
+  },
+  {
+    "revision": "56c5a99682aed5776805",
+    "url": "/static/js/2.ceee5015.chunk.js"
+  },
+  {
+    "revision": "6cff9ed4c33b6c043ebceb71aa11fbb2",
+    "url": "/static/js/2.ceee5015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116d9c29d8578bfb29d6",
+    "url": "/static/js/main.46f0bb95.chunk.js"
+  },
+  {
+    "revision": "0e1e545eb08084489d8b",
+    "url": "/static/js/runtime-main.af5f005b.js"
+  },
+  {
+    "revision": "1335ae8bad7d6decd211f0cc5ed664ad",
+    "url": "/static/media/arrow-down-black.1335ae8b.svg"
+  },
+  {
+    "revision": "f9f59cb6b0557f57c5f0ddea1124598c",
+    "url": "/static/media/arrow-down.f9f59cb6.svg"
+  },
+  {
+    "revision": "9c54c8eaaf3ad04d8c391755618bf509",
+    "url": "/static/media/arrow-up-yellow.9c54c8ea.svg"
+  },
+  {
+    "revision": "caa2d3a66ec6c35e6449f40410242ff8",
+    "url": "/static/media/arrow-up.caa2d3a6.svg"
+  },
+  {
+    "revision": "abf234bbc9834027542666b248725b1f",
+    "url": "/static/media/bonus.abf234bb.gif"
+  },
+  {
+    "revision": "9b5049a4312218d620c3192400e838a8",
+    "url": "/static/media/btn-back.9b5049a4.svg"
+  },
+  {
+    "revision": "952eca8e411cbaae494b5454074cc4dd",
+    "url": "/static/media/card-bottom.952eca8e.svg"
+  },
+  {
+    "revision": "1924b5bd6aa59db8faa454bf265d85ba",
+    "url": "/static/media/fail.1924b5bd.gif"
+  },
+  {
+    "revision": "e88a45c40ae4822a8aaed46f20b60563",
+    "url": "/static/media/gift.e88a45c4.gif"
+  },
+  {
+    "revision": "cb1d959571aeeef4d186afd616f7b9ec",
+    "url": "/static/media/icon-about-us.cb1d9595.svg"
+  },
+  {
+    "revision": "9b971c233c7d81abc3f7c1ca9b2f00cb",
+    "url": "/static/media/icon-activity-participation.9b971c23.svg"
+  },
+  {
+    "revision": "034c667193c041cd18317c779defa775",
+    "url": "/static/media/icon-brand-checked.034c6671.svg"
+  },
+  {
+    "revision": "c48f84d464e2fb947f522f54b80d2a93",
+    "url": "/static/media/icon-crown-black.c48f84d4.svg"
+  },
+  {
+    "revision": "497b178bf5ce1dc34f7f87fcd391b211",
+    "url": "/static/media/icon-crown.497b178b.svg"
+  },
+  {
+    "revision": "99b6b631b474d6ede00f7625fa27f7e1",
+    "url": "/static/media/icon-earn-points-history.99b6b631.svg"
+  },
+  {
+    "revision": "cdc20ac9b113ccb4089e64ab26dfdc2c",
+    "url": "/static/media/icon-earn-points.cdc20ac9.svg"
+  },
+  {
+    "revision": "73d6073be198fc0ceff067d8c620fd8e",
+    "url": "/static/media/icon-events.73d6073b.svg"
+  },
+  {
+    "revision": "a3b9431c8755f813cdcd262afc64f158",
+    "url": "/static/media/icon-filter-highlight.a3b9431c.svg"
+  },
+  {
+    "revision": "30833649607ffa03045d8917afd99466",
+    "url": "/static/media/icon-filter.30833649.svg"
+  },
+  {
+    "revision": "146c39de861a93b88bac73846c0022de",
+    "url": "/static/media/icon-gift-redemption.146c39de.svg"
+  },
+  {
+    "revision": "0f60c5aa9f43d90f3702af75af11be8d",
+    "url": "/static/media/icon-gifts.0f60c5aa.svg"
+  },
+  {
+    "revision": "64819a48c1fa5f38b6e39c0e641db436",
+    "url": "/static/media/icon-home.64819a48.svg"
+  },
+  {
+    "revision": "3fbde65118d73f01f1fea465d2f258c3",
+    "url": "/static/media/icon-logout.3fbde651.svg"
+  },
+  {
+    "revision": "703a6d35e8b6f8a18740b404ed615f63",
+    "url": "/static/media/icon-message.703a6d35.svg"
+  },
+  {
+    "revision": "af5258a1386696312d56120767bfe679",
+    "url": "/static/media/icon-minus.af5258a1.svg"
+  },
+  {
+    "revision": "c51fba160258ae42826e5c06285055d4",
+    "url": "/static/media/icon-my-profile.c51fba16.svg"
+  },
+  {
+    "revision": "313fe3eb0ef24d60b0cfab214ddb7c25",
+    "url": "/static/media/icon-my-redemption.313fe3eb.svg"
+  },
+  {
+    "revision": "71347a0d828c5784a7c0016fa0ce8fb1",
+    "url": "/static/media/icon-page-title.71347a0d.svg"
+  },
+  {
+    "revision": "86207d720431dc8eaa4c0d7182844602",
+    "url": "/static/media/icon-plus.86207d72.svg"
+  },
+  {
+    "revision": "49d16c18553157bdd07fb7b6b3030bfc",
+    "url": "/static/media/icon-points-balance.49d16c18.svg"
+  },
+  {
+    "revision": "15f00cedb35d8b7255cbd2fbb08ed53a",
+    "url": "/static/media/icon-qrcode-arrow-lb.15f00ced.svg"
+  },
+  {
+    "revision": "9f260f04a186c2496ba3b763d7e14050",
+    "url": "/static/media/icon-qrcode-arrow-lt.9f260f04.svg"
+  },
+  {
+    "revision": "b84c65d4b0a5c0c573b209abb33bbc4b",
+    "url": "/static/media/icon-qrcode-arrow-rb.b84c65d4.svg"
+  },
+  {
+    "revision": "3dbf73cdbf783f5c7702f7255db91882",
+    "url": "/static/media/icon-qrcode-arrow-rt.3dbf73cd.svg"
+  },
+  {
+    "revision": "bb05798a2ce0032efb9255796c706184",
+    "url": "/static/media/icon-receipt-upload-calendar.bb05798a.svg"
+  },
+  {
+    "revision": "034c667193c041cd18317c779defa775",
+    "url": "/static/media/icon-remove-file.034c6671.svg"
+  },
+  {
+    "revision": "8a390df8acc3dfc34ec9d460739b6ac3",
+    "url": "/static/media/icon-reward-adjust.8a390df8.svg"
+  },
+  {
+    "revision": "5cbcca542a2c5ac580ed88e3494d2627",
+    "url": "/static/media/icon-spending-reward.5cbcca54.svg"
+  },
+  {
+    "revision": "d45ce08f803fbb15b4a4ff4fd76214c0",
+    "url": "/static/media/icon-tick.d45ce08f.svg"
+  },
+  {
+    "revision": "9216849db1abe389524976e827ab4bc6",
+    "url": "/static/media/icon-top-points-balance.9216849d.svg"
+  },
+  {
+    "revision": "8dc60a572428264689093a39d837c9b5",
+    "url": "/static/media/icon-transaction-history.8dc60a57.svg"
+  },
+  {
+    "revision": "710722e9e26424b7100cb644a719cbb5",
+    "url": "/static/media/icon-upload.710722e9.svg"
+  },
+  {
+    "revision": "625fab82c6e303587eb31370ba6e5f8c",
+    "url": "/static/media/icon-user-profile.625fab82.svg"
+  },
+  {
+    "revision": "864a65b712ea8f59ec25e60c36717a47",
+    "url": "/static/media/icon-user-qrcode.864a65b7.svg"
+  },
+  {
+    "revision": "0a8ef4910ef1fc43f8face172a6bc177",
+    "url": "/static/media/icon-user-redemption.0a8ef491.svg"
+  },
+  {
+    "revision": "2916eaf2d9427b5e647ca3349d44e5a5",
+    "url": "/static/media/logo-2.2916eaf2.svg"
+  },
+  {
+    "revision": "49c5d5d5186365d72cfb32ebab8db266",
+    "url": "/static/media/logo.49c5d5d5.svg"
+  },
+  {
+    "revision": "f7c6ea3482a638298661a4491fe8f257",
+    "url": "/static/media/meLogo.f7c6ea34.svg"
+  },
+  {
+    "revision": "6ffcbc00205dde84b7bfdd65d3c57b59",
+    "url": "/static/media/mygift_empty.6ffcbc00.gif"
+  },
+  {
+    "revision": "e60499351cd098c8d226fa461e0d8039",
+    "url": "/static/media/no_bonus.e6049935.gif"
+  },
+  {
+    "revision": "82243fb4006eef2d58e33c67e970f0d9",
+    "url": "/static/media/no_gift_ticket.82243fb4.gif"
+  },
+  {
+    "revision": "526d7fdf63614222d376257221e8b754",
+    "url": "/static/media/slick.526d7fdf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "7c445ee14cd9f63fc65d323608354872",
+    "url": "/static/media/success.7c445ee1.gif"
+  },
+  {
+    "revision": "b7db9423510023ed47f36c6eae4cc851",
+    "url": "/static/media/ticket.b7db9423.gif"
+  }
+]);
